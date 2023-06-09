59f42450c4b41badfcb3550f1f8863e7302f4037
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-06-09 19:07:59 -0300

    hash da senha e validacao

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -8,14 +9,26 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         email: z.string().email(),
         password: z.string().min(6),
     })
-    
+
     const { name, email, password } = registerBodySchema.parse(request.body)
+
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {      
+            email,
+        },
+    })
+
+    if(userWithSameEmail) {
+        return reply.status(409).send({})
+    }
 
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password,
+            password_hash,
         },
     })
 
