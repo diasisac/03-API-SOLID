4e1966250b03aa1646605d5b7ec1cc7b6ca7bed3
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-06-09 14:33:40 -0300

    fundamentos do prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Isac Dias',
+        email: 'isac_dp1@hotmail.com'
+    }
+})
