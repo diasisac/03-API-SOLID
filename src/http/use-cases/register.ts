cdd2a08522dd7be7fea054be9110592951e756fd
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-06-10 00:16:56 -0300

    inversao de dependencia

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,31 +1,29 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 interface RegisterUseCaseRequest {
     name: string
     email: string
     password: string
 }
+export class RegisterUseCase {
+    constructor(private usersRepository: any) { }
+    async execute({ name, email, password }: RegisterUseCaseRequest) {
+        const password_hash = await hash(password, 6)
 
+        const userWithSameEmail = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        })
 
-export async function registerUseCase({name, email, password}: RegisterUseCaseRequest){
-    const password_hash = await hash(password, 6)
+        if (userWithSameEmail) {
+            throw new Error('User already exists')
+        }
 
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {      
+        await this.usersRepository.create({
+            name,
             email,
-        },
-    })
-
-    if(userWithSameEmail) {
-        throw new Error('User already exists')
+            password_hash,
+        })
     }
-
-    const prismaUsersRepository = new PrismaUsersRepository()
-
-    await prismaUsersRepository.create({
-        name,
-        email,
-        password_hash,
-    })
 }
