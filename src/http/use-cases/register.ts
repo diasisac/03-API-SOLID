c156962eca40d0d8fe49fae4216e2c5f7e8058f3
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-06-10 00:46:22 -0300

    lidando com erros do use case

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '@/repositories/usersRepository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 interface RegisterUseCaseRequest {
     name: string
     email: string
@@ -14,7 +15,7 @@ export class RegisterUseCase {
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
         if (userWithSameEmail) {
-            throw new Error('User already exists')
+            throw new UserAlreadyExistsError()
         }
 
         await this.usersRepository.create({
