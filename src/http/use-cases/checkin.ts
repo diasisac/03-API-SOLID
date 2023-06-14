cba88b40eac5226295383728ec6782fcb032c6e3
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-06-13 23:21:34 -0300

    caso de uso checkin

---
+++ src/http/use-cases/checkin.ts
@@ -0,0 +1,26 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/checkinsRepository'
+
+interface CheckInUseRequest {
+    userId: string
+    gymId: string
+}
+
+interface CheckInUseResponse {
+    checkin: CheckIn
+}
+
+export class CheckInUse {
+    constructor(private readonly checkinsRepository: CheckInsRepository) { }
+
+    async execute({ userId, gymId }: CheckInUseRequest): Promise<CheckInUseResponse> {
+        const checkin = await this.checkinsRepository.create({
+            user_id :userId,
+            gim_id: gymId
+        })
+
+        return {
+            checkin
+        }
+    }
+}
